3bb0857b692c73870e89a2b1c0e505329574b416
Pranav Nigam <pranav.nigs@gmail.com>
2020-05-18 18:22:55 +0530

    chart color updated

--- src/app/charts/common-chart/common-chart.component.ts
+++ src/app/charts/common-chart/common-chart.component.ts
@@ -9,7 +9,7 @@ import { SocketService } from 'src/app/socket.service';
 export class CommonChartComponent implements OnInit {
 
   chart;
-  chartType: string = 'doughnut';
+  chartType: string = 'bar';
   constructor(private srv : SocketService) { }
 
   ngOnInit(): void {
@@ -35,14 +35,14 @@ export class CommonChartComponent implements OnInit {
             label: 'Test Chart',
             fill: false,
             backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(255, 206, 86, 0.2)',
-              'rgba(75, 192, 192, 0.2)',
-              'rgba(153, 102, 255, 0.2)',
-              'rgba(255, 159, 64, 0.2)',
-              'rgba(255, 112, 64, 0.2)',
-              'rgba(255, 180, 64, 0.2)'
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)',
+              'rgba(255, 112, 64, 1)',
+              'rgba(255, 180, 64, 1)'
             ],
             borderColor: [
               'rgba(255, 99, 132, 0.2)',
